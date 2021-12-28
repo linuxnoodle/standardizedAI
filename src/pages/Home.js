bf98fe11a33550ce052f66db9b9982b744cd9f5b
Farhan Sheikh <linuxnoodle:Trc8github@github.com>
2021-12-28 15:55:35 -0500

    Added home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import "../App.css"
 
 const Home = () => {
     return (
-        <p>AMONG US</p>
+        <div class="App">
+            <div class="App-nav">
+                <span class="App-nav-title">standardizedAI</span>
+                <span class="App-nav-button">
+                    <a href="#">Sign In With Google</a>
+                </span>
+            </div>
+            <div class="App-header">
+                <div>Welcome to standardizedAI!</div>
+            </div>
+            <div class="AppBody">
+                among us sus
+            </div> 
+        </div>
     );
 };
 
